8986697510b056ae58b23626944073eed2f285d1
jojusuar <jojusuar@espol.edu.ec>
2024-01-13 16:48:55 -0500

    correccion en la logica de la utilidad

--- src/main/java/modelo/TestZone.java
+++ src/main/java/modelo/TestZone.java
@@ -1,10 +1,5 @@
 package modelo;
 
-
-import estructuras.Tree;
-import modelo.Board;
-import modelo.MinMaxer;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -17,8 +12,5 @@ import modelo.MinMaxer;
 public class TestZone {
     public static void main(String[] args){
         Board emptyBoard = new Board();
-        Tree<Board> tree = MinMaxer.minmax(emptyBoard);
-        
-        System.out.println(emptyBoard.utilityFunction(Circle.class));
     }
 }
